277e9d1cb5df43517ad3d75a9e3ff84fce0d63f4
Saad Nafees <saadnafees@gmail.com>
2026-08-05 14:52:42 +0500

    feat(core): establish scalable project architecture

--- lib/app/theme/app_radius.dart
+++ lib/app/theme/app_radius.dart
@@ -2,8 +2,12 @@ class AppRadius {
   AppRadius._();
 
   static const double sm = 8;
+
   static const double md = 12;
+
   static const double lg = 16;
+
   static const double xl = 24;
+
   static const double round = 999;
 }
